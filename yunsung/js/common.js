b3e432df787b4b841a011f51252c86d1d7243087
ajjsss7899 <ajjsss7899@naver.com>
2016-08-17 23:17:14 +0900

    dfdf

--- yunsung/js/common.js
+++ yunsung/js/common.js
@@ -1,47 +1,35 @@
 // gnb script
 $(function(){
-	$(window).resize(function(){
-		var $gnbWrap = $('.gnb'),
-		$gnbChild = $('.gnb > li > ul');
-		if ($(window).width() > 1005) {
-			$gnbWrap.on('mouseenter focusin', function(){
-			$gnbChild.addClass('on'),
-			$gnbWrap.css("border-bottom"," 1px solid #c6c6c6")
-		});
-			$gnbWrap.on('mouseleave', function(){
-			$gnbChild.removeClass('on'),
-			$gnbWrap.css("border-bottom"," 1px solid #e1e1e1")
-		});
-			$('.srch').on('focusin', function(){
-			$gnbChild.removeClass('on')
-			});
-		}else{
-			$gnbWrap.on('mouseenter focusin', function(){
-			$gnbChild.removeClass('on')
-		});
-			$('.srch').on('focusin', function(){
-			$gnbChild.removeClass('on')
-			});
-		}
-	}).resize();
+	$('.gnb').on('mouseenter focusin',function(){
+		$('.gnb > li > ul').addClass('on');
+		$('.gnb').css('border-bottom','1px solid #c6c6c6');
+	});
+	$('.gnb').on('mouseleave', function(){
+		$('.gnb > li > ul').removeClass('on');
+		$('.gnb').css('border-bottom','1px solid #e1e1e1');
+	});
+	$('.srch').on('focusin',function(){
+		$('.gnb > li > ul').removeClass('on');
+		$('.gnb').css('border-bottom','1px solid #e1e1e1');
+	});
 });
 
 // 모바일 gnb script
 $(function(){
 	$(".sitemap").on("click", function(){
-		$(".m_nav, .util_menu").animate({left:270}, 300);
-		$(".m_nav, .util_menu, .gnb, .dark").addClass("on");
+		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({left:270}, 300);
+		$(".m_gnb_wrap, .util_menu, .dark").addClass("on");
 		$("html,body").addClass("scroll");
 	});
 	$(".close, .dark").on("click", function(){
-		$(".m_nav, .util_menu").animate({left:0}, 300);
+		$(".m_gnb_wrap h1, .util_menu").animate({left:0}, 300);
 		setTimeout(function(){
-		$(".m_nav, .util_menu, .gnb, .dark").removeClass("on");
+		$(".m_gnb_wrap, .util_menu, .dark").removeClass("on");
 		},300);
 		$("html,body").removeClass("scroll");
 
 	});
-	$(".gnb > li").on('click', function(){
+	$(".m_gnb > li").on('click', function(){
 		var tab = $(this);
 		if(tab.hasClass("on"));
 		tab.addClass("on").siblings(".on").removeClass("on");
