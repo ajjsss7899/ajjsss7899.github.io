ec252a9b5c323a54d6efa30205fd16cfcea99702
ajjsss7899 <ajjsss7899@naver.com>
2016-08-23 15:48:35 +0900

    dss

--- yunsung/js/common.js
+++ yunsung/js/common.js
@@ -17,12 +17,12 @@ $(function(){
 // 모바일 gnb script
 $(function(){
 	$(".sitemap").on("click", function(){
-		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({left:270}, 300);
+		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({left:0}, 300);
 		$(".m_gnb_wrap, .util_menu, .dark").addClass("on");
 		// $("html,body").addClass("scroll");
 	});
 	$(".close, .dark").on("click", function(){
-		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({left:0}, 300);
+		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({left:-270}, 300);
 		setTimeout(function(){
 		$(".m_gnb_wrap, .util_menu, .dark").removeClass("on");
 		},300);
