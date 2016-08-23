218511ca2afe028671b32128543674342be88d60
ajjsss7899 <ajjsss7899@naver.com>
2016-08-23 18:15:38 +0900

    dfdf

--- yunsung/js/common.js
+++ yunsung/js/common.js
@@ -15,13 +15,31 @@ $(function(){
 });
 
 // 모바일 gnb script
+// $(function(){
+// 	$(".sitemap").on("click", function(){
+// 		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({"left":"+=270px"}, 300);
+// 		$(".util_menu, .dark").addClass("on");
+// 	});
+// 	$(".close, .dark").on("click", function(){
+// 		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({"left":"-=270px"}, 300);
+// 		setTimeout(function(){
+// 		$(".util_menu, .dark").removeClass("on");
+// 		},300);
+// 	});
+// 	$(".m_gnb > li").on('click', function(){
+// 		var tab = $(this);
+// 		if(tab.hasClass("on"));
+// 		tab.addClass("on").siblings(".on").removeClass("on");
+// 	});
+// });
+
 $(function(){
 	$(".sitemap").on("click", function(){
-		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({"left":"+=270px"}, 300);
-		$(".util_menu, .dark").addClass("on");
+
+		$(".m_gnb, .m_gnb_wrap h1, .util_menu .util_menu, .dark").addClass("on");
 	});
 	$(".close, .dark").on("click", function(){
-		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({"left":"-=270px"}, 300);
+		$(".m_gnb, .m_gnb_wrap h1, .util_menu .util_menu, .dark").removeClass("on");
 		setTimeout(function(){
 		$(".util_menu, .dark").removeClass("on");
 		},300);
@@ -32,7 +50,6 @@ $(function(){
 		tab.addClass("on").siblings(".on").removeClass("on");
 	});
 });
-
 // gnb fixed
 $(window).scroll(function(){
 	if($(window).scrollTop() >=40){
