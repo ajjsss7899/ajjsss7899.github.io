63da359f28263f2571095df232c6076f5c43b025
ajjsss7899 <ajjsss7899@naver.com>
2016-08-23 19:16:21 +0900

    dfdf

--- yunsung/js/common.js
+++ yunsung/js/common.js
@@ -19,14 +19,14 @@ $(function(){
 	$(".sitemap").on("click", function(){
 		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({"left":"+=270px"}, 300);
 		$(".util_menu, .dark").addClass("on");
-		$(".m_gnb").addClass("on");
+		$("html,body").addClass("scroll");
 	});
 	$(".close, .dark").on("click", function(){
 		$(".m_gnb, .m_gnb_wrap h1, .util_menu").animate({"left":"-=270px"}, 300);
 		setTimeout(function(){
 		$(".util_menu, .dark").removeClass("on");
+		$("html,body").removeClass("scroll");
 		},300);
-		$(".m_gnb").removeClass("on");
 	});
 	$(".m_gnb > li").on('click', function(){
 		var tab = $(this);
