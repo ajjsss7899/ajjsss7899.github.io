f3ef04e3a2f81db35cc18e2558231fe98bf7d615
ajjsss7899 <ajjsss7899@naver.com>
2016-10-13 00:50:11 +0900

    dfdf

---
+++ daejeon/js/common.js
@@ -0,0 +1,22 @@
+// 글로벌 네비게이션 script
+// $(function(){
+//     var $gnbWrap = $('.gnb'),
+// 		$gnbChild = $('.gnb > li > ul');
+//     $gnbWrap.on('mouseenter focusin', function(){
+// 		$gnbChild.stop().slideDown(400);
+// 	});
+//     $gnbWrap.on('mouseleave', function(){
+//         $gnbChild.stop().slideUp(400);
+// 	});
+//     $('.last').focusout(function(){
+//    		$gnbChild.stop().slideUp(400);
+// 	});
+// });
+
+// $(function(){
+//     $(".lnb li").on('click focusin', function(){
+//         if($(this).hasClass("on"));
+//         $(this).addClass("on").siblings(".on").removeClass("on");
+//     });
+// });
+
